82a2fe83f1d5ca0ee9829cdfde379e0ca1d9677e
noreddine1 <62399393+noreddine1@users.noreply.github.com>
2022-11-19 18:16:09 +0100

    commit ft_print.h

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaazouz <nmaazouz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 18:09:20 by nmaazouz          #+#    #+#             */
+/*   Updated: 2022/11/19 18:09:21 by nmaazouz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int n);
+int	ft_puthexa(unsigned long long n, char c);
+int	ft_printf(const char *format, ...);
+
+#endif
